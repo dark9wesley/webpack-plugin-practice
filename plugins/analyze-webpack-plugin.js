2da63fb945e34c8df2cb49c721aca93c96111d7b
wesley <877285030@qq.com>
2022-06-24 15:59:39 +0800

    feat: analyze-webpack-plugin

---
+++ plugins/analyze-webpack-plugin.js
@@ -0,0 +1,21 @@
+class AnalyzeWebpackPlugin {
+  apply(compiler){  
+    compiler.hooks.emit.tap('AnalyzeWebpackPlugin', (compilation) => {
+      // Object.entries将对象变成二维数组。二维数组中第一项值是key，第二项值是value
+      const assets = Object.entries(compilation.assets)
+
+      let source = '# 分析打包资源大小 \n| 名称 | 大小 |\n| --- | --- |'
+      assets.forEach(([filename, file]) => {
+        source += `\n| ${filename} | ${file.size()} |`;
+      })
+
+      //添加资源
+      compilation.assets["analyze.md"] = {
+        source: () => source,
+        size: () => source.length,
+      }
+    })
+  }
+}
+
+module.exports = AnalyzeWebpackPlugin;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 // const TestPlugin = require('./plugins/test-plugin')
 const BannerPlugin = require('./plugins/banner-webpack-plugin')
 const CleanPlugin = require('./plugins/clean-webpack-plugin')
+const AnalyzePlugin = require('./plugins/analyze-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -15,7 +16,8 @@ module.exports = {
     new BannerPlugin({
       author: 'pengyw'
     }),
-    new CleanPlugin()
+    new CleanPlugin(),
+    new AnalyzePlugin(),
   ],
   mode: 'production',
 }
